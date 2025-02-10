0c91aa81b0b8645ac913df44298728b204325884
Ana Carolina Ribeiro <carolribeiro2112@gmail.com>
2025-02-10 10:13:43 -0300

    feat: Create login route

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-from flask_login import UserMixin
+from flask_login import UserMixin, login_user, LoginManager
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "minha_chave_123"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
 
+login_manager = LoginManager()
 db = SQLAlchemy(app)
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 CORS(app)
 
 #Usuário
@@ -14,6 +18,18 @@ class User(db.Model, UserMixin):
   id = db.Column(db.Integer, primary_key=True)
   username = db.Column(db.String(80), nullable=False, unique=True)
   password = db.Column(db.String(80), nullable=False)
+
+@app.route('/login', methods=["POST"])
+def login():
+  data = request.json
+  user = User.query.filter_by(username=data.get("username")).first()
+
+  if user and data.get("password") == user.password:
+    login_user(user)
+    return jsonify({"message": "Logged in successfully"}) 
+  
+  return jsonify({"message": "Unauthorized. Invalid credentials"}), 401
+
 
 #Produto
 class Product(db.Model):
